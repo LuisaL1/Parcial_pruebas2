ea6cc391b06ffcbca7a0c5b1777068482bef1aa2
Luisa Londoño <mlondono408@cue.edu.co>
2026-06-05 18:06:39 -0500

    GREEN: aplicar bonificacion del 25 por ciento

--- app/service.py
+++ app/service.py
@@ -32,3 +32,22 @@ def calcular_recarga(monto, premium):
         "valida": True,
         "bonificacion": bonificacion
     }
+def calcular_recarga(monto, premium):
+
+    if monto < 1000 or monto > 50000:
+        return {"valida": False}
+
+    bonificacion = 0
+
+    if monto >= 30000:
+        bonificacion = 25
+
+    elif monto >= 10000:
+        bonificacion = 10
+
+    return {
+        "valida": True,
+        "bonificacion": bonificacion
+    }
+
+
