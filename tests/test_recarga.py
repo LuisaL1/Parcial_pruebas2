bd066f4471481692eddb1d8fc9b5845077f79184
Luisa Londoño <mlondono408@cue.edu.co>
2026-06-05 18:09:35 -0500

    RED: test fallido para usuario premium

--- tests/test_recarga.py
+++ tests/test_recarga.py
@@ -17,3 +17,9 @@ def test_recarga_30000():
     resultado = calcular_recarga(30000, False)
 
     assert resultado["bonificacion"] == 25
+
+def test_usuario_premium():
+
+    resultado = calcular_recarga(30000, True)
+
+    assert resultado["bonificacion"] == 30
